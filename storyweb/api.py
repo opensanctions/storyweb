00dbbad5b114d6093fbadf74a85791e183f0efba
Friedrich Lindenberg <friedrich@pudo.org>
2022-09-29 14:47:57 +0200

    still not sure how to do this

--- storyweb/api.py
+++ storyweb/api.py
@@ -1,4 +1,22 @@
 # * get the set of (unprocessed) tags (site/ilike search)
+#
+# /tags/?site=xxxx&q=putin
+# {'text', 'texts', 'key', 'ref_id', 'ref_site', 'ref_title', 'ref_url', 'identity_id', 'identity_cluster_id'}
+#
+# /identities/?q=putin
+# {'cluster_id', 'identity_ids', 'text', 'texts'}
+#
+# POST /identities <- {'key', 'ref_id'}
+#
+# /identities/xxxx
+# {'cluster_id', 'text', 'tags': [{}], }
+#
+# /identities/xxxx/cooccuring?unlinked=true
+# {'key', 'refs', 'count'}
+#
+# /identities/xxxx/links  (or /link?identity=xxxx ?)
+# {'source_id', 'target_id', 'type', ...}
+#
 # * view an identity
 #   * see all possible aliases (same name, different article tags)
 #   * see all possible links
